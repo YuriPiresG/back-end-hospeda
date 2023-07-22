9f2cf49a61ad873227a34c0865d74dbffc0aa6bc
Yuri Pires <yuripiresbr@gmail.com>
2023-07-21 22:14:00 -0300

    feat: created activity entity

--- src/activities/entities/activity.entity.ts
+++ src/activities/entities/activity.entity.ts
@@ -1,1 +1,44 @@
-export class Activity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Privacy } from '../constants/privacy.constant';
+
+@Entity()
+export class Activity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  privacy: Privacy;
+
+  @Column()
+  description: string;
+
+  @Column()
+  cep: string;
+
+  @Column()
+  streetNumber: number;
+
+  @Column()
+  address: string;
+
+  @Column()
+  additionalInfo: string;
+
+  @Column()
+  neighborhood: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column({ type: 'date' })
+  initialDate: Date;
+
+  @Column({ type: 'timestamptz' })
+  initialHour: Date;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ActivitiesModule } from './activities/activities.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Activity } from './activities/entities/activity.entity';
 
 @Module({
-  imports: [ActivitiesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin123',
+      database: 'activitiesManager',
+      entities: [Activity],
+      synchronize: true,
+    }),
+    ActivitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
